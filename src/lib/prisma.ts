6903721c7ec972e419111ffac098a01f0efee59d
guskirb <anguskirby1998@gmail.com>
2024-08-08 16:56:03 +0100

    Add joined date formatted to prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,5 +1,19 @@
 import { PrismaClient } from "@prisma/client";
+import { DateTime } from "luxon";
 
 const globalForPrism = global as unknown as { prism: PrismaClient };
 
-export const prisma = globalForPrism.prism || new PrismaClient();
+export const prisma =
+  globalForPrism.prism ||
+  new PrismaClient().$extends({
+    result: {
+      user: {
+        joinedFormatted: {
+          needs: { joinedAt: true },
+          compute(user) {
+            return DateTime.fromJSDate(user.joinedAt).toFormat("MMMM yyyy");
+          },
+        },
+      },
+    },
+  });
